26ad2828a2328de8a7ef160c7ec04ae4d9760fcf
MuckT <mcgtom10@gmail.com>
2021-02-13 20:32:05 -0800

    test: reduce code duplication on viewport spec

--- cypress/integration/viewport.spec.js
+++ cypress/integration/viewport.spec.js
@@ -1,62 +1,37 @@
 /// <reference types="cypress" />
 
-describe("desktop-tests", () => {
-  it("adds a session from list to favorites", () => {
-    cy.disableTutorial();
-    cy.contains("University of Ionic").click({ force: true });
-    cy.get("[data-cy=favorite]").click();
-    cy.get("[data-cy=back").click();
-    cy.contains("Favorites").click();
-    cy.contains("University of Ionic");
-  });
+const sizes = ['iphone-5', 'macbook-15'];
+sizes.forEach(size => {
+  describe(`${size} Tests`, () => {
+    beforeEach(() => {
+      cy.viewport(size);
+      cy.disableTutorial();
+    });
 
-  it("enables dark mode", () => {
-    cy.disableTutorial();
-    cy.get("[data-cy=dark-mode]").click();
-    cy.contains("Dark Mode").should("have.css", "color", "rgb(255, 255, 255)");
-  });
+    it("Adds A Session From List To Favorites", () => {
+      cy.contains("University of Ionic").click({ force: true });
+      cy.get("[data-cy=favorite]").click();
+      cy.get("[data-cy=back").click();
+      cy.contains("Favorites").click();
+      cy.contains("University of Ionic");
+    });
 
-  it("searches for a session and adds to favorites", () => {
-    cy.disableTutorial();
-    cy.get("[data-cy=search]").click();
-    cy.get("[data-cy=searchbar]").click().type("Angular{enter}");
-    cy.contains("Angular Directives").click();
-    cy.get("[data-cy=favorite]").click();
-    cy.get("[data-cy=back]").click();
-    cy.contains("Favorites").click();
-    cy.contains("Angular Directives");
-  });
-});
+    it("Enables Dark Mode", () => {
+      if (size === 'iphone-5') {
+        cy.get('[data-cy="menu"]').click();
+      }
+      cy.get("[data-cy=dark-mode]").click();
+      cy.contains("Dark Mode").should("have.css", "color", "rgb(255, 255, 255)");
+    });
 
-describe("mobile-tests", () => {
-  beforeEach(() => {
-    cy.viewport("iphone-5");
-  });
-
-  it("adds a session from list to favorites", () => {
-    cy.disableTutorial();
-    cy.contains("University of Ionic").click({ force: true });
-    cy.get("[data-cy=favorite]").click();
-    cy.get("[data-cy=back").click();
-    cy.contains("Favorites").click();
-    cy.contains("University of Ionic");
-  });
-
-  it("enables dark mode", () => {
-    cy.disableTutorial();
-    cy.get("[data-cy=menu]").click();
-    cy.get("[data-cy=dark-mode]").click();
-    cy.contains("Dark Mode").should("have.css", "color", "rgb(255, 255, 255)");
-  });
-
-  it("searches for a session and adds to favorites", () => {
-    cy.disableTutorial();
-    cy.get("[data-cy=search]").click();
-    cy.get("[data-cy=searchbar]").click().type("Angular{enter}");
-    cy.contains("Angular Directives").click();
-    cy.get("[data-cy=favorite]").click();
-    cy.get("[data-cy=back]").click();
-    cy.contains("Favorites").click();
-    cy.contains("Angular Directives");
+    it("Searches For A Session & Adds To Favorites", () => {
+      cy.get("[data-cy=search]").click();
+      cy.get("[data-cy=searchbar]").click().type("Angular{enter}");
+      cy.contains("Angular Directives").click();
+      cy.get("[data-cy=favorite]").click();
+      cy.get("[data-cy=back]").click();
+      cy.contains("Favorites").click();
+      cy.contains("Angular Directives");
+    });
   });
 });
